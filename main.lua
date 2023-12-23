2ebf94a6e75c55fb0ddfd9bc8fbb99150aafb168
anhvu3321 <117070253+anhvu3321@users.noreply.github.com>
2023-12-23 10:07:44 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
-
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+for i, v in pairs(game.Players.LocalPlayer.PlayerGui:FindFirstChild("Notifications"):GetChildren()) do
+    if v:IsA("TextLabel") and child.Name == "NotificationTemplate" then
+        if string.find(v.Text, "QUEST COMPLETED") then
+            OrionLib:MakeNotification({
+                Name = "Status",
+                Content = "Quest Done",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
+        end
+    end
+end

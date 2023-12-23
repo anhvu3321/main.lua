bc23a6274c8ada692a36cbb3e00158e6d5b85324
anhvu3321 <117070253+anhvu3321@users.noreply.github.com>
2023-12-23 10:09:04 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+print("Script executed")
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 for i, v in pairs(game.Players.LocalPlayer.PlayerGui:FindFirstChild("Notifications"):GetChildren()) do
     if v:IsA("TextLabel") and child.Name == "NotificationTemplate" then

593e5637596e913ae8ab12c0ce8d731952f3183b
anhvu3321 <117070253+anhvu3321@users.noreply.github.com>
2023-12-23 10:14:04 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 print("Script executed")
-for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Notifications"):GetChildren()) do
-    if v:IsA("TextLabel") and v.Name == "NotificationTemplate" then
-        if string.find(v.Text, "QUEST COMPLETED") then
-            print("Quest done")
+while wait() do
+    for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Notifications"):GetChildren()) do
+        if v:IsA("TextLabel") and v.Name == "NotificationTemplate" then
+            if string.find(v.Text, "QUEST COMPLETED") then
+                print("Quest done")
+            end
         end
     end
 end

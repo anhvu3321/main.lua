d851f4a7a6612198b3c88799aedb52ed87b34400
anhvu3321 <117070253+anhvu3321@users.noreply.github.com>
2023-12-29 17:43:52 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,93 +1,102 @@
-getgenv().Setting = {
-    ["Team"] = "Pirates", --[[Marines]]
-    ["Webhook"] = {
-        ["Url"] = "https://discord.com/api/webhooks/1160607946313568417/66MIAVj7-POIFvHLDPu4zpisGyJUl8fcR0jTJl2T0yteZASXG8V9OcFvmPfRZj7Uk6_Y", --[[ link webhook]]
-        ["Enabled"] = true, --[[ enable webhook]]
-    },
-    ["BypassTP"] = {
-        ["Enable"] = true, --[[ bypass tp ]]
-        ["Attempt"] = 0, --[[ Tween If Failed After x Attempts (change to 0 for inf bypass tp even it fail)]]
-    },
-    ["FpsLock"] = {
-        ["Enable"] = true, --[[ lock fps]]
-        ["Cap"] = 30, --[[ fps to lock]]
-    },
-    ["LockBounty"] = {
-        ["Enable"] = true, --[[ lock bounty]]
-        ["Cap"] = 30000000, --[[ reach to this bounty do the action below]]
-        ["Action"] = "Kick", --[[ Kick, Shutdown]]
-        ["SendMessage"] = true, --[[ send message to webhook when reach the cap]]
-        ["Message"] = "Congratulation You Have Reached The Bounty Cap MyBounty" --[[ It Will Replace MyBounty With Your Current Bounty, Add Ping Everyone If You Want]]
-    },
-    ["Click"] = {
-        ["Enable"] = true, --[[ click ]]
-        ["FastClick"] = true --[[ fast click]]
-    },
-    ["Haki Ken"] = {
-        ["Enable"] = true, --[[ Ken Haki ]]
-    },
-    ["SpamSkill"] = true, --[[ Will use all skills as fast as possbile ignore holding skills]]
-    ["Weapons"] = { --[[ Select Weapon, Self Explain]]
-        ["Melee"] = {
-            ["Enable"] = true, --[[ enable using melee]]
-            ["Delay"] = 1,   --[[ time delay between the melee skill]] 
-            ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = true, --[[ enable using skill Z]]
-                    ["HoldTime"] = 0,  --[[ hold skill in (seconds)]]
-                },
-                ["X"] = {
-                    ["Enable"] = true, --[[ enable using skill X ]]
-                    ["HoldTime"] = 0,  --[[ hold skill in (seconds)]]
-                },
-
-                ["C"] = {
-                    ["Enable"] = true, --[[ enable using skill C]]
-                    ["HoldTime"] = 0,  --[[ hold skill in (seconds)]]
-                },
-            },
-        },
-        ["Blox Fruit"] = {
-            ["Enable"] = false, --[[ enable using blox fruit]]
-            ["Delay"] = 0,  --[[ time delay between the blox fruit skill ]]
-            ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = true, --[[ enable using skill Z]]
-                    ["HoldTime"] = 0, --[[ hold skill in (seconds)]]
-                },
-                ["X"] = {
-                    ["Enable"] = true, --[[ enable using skill X]]
-                    ["HoldTime"] = 0, --[[ hold skill in (seconds)]]
-                },
-
-                ["C"] = {
-                    ["Enable"] = true, --[[ enable using skill C]]
-                    ["HoldTime"] = 1, --[[ hold skill in (seconds)]]
-                },
-                ["V"] = {
-                    ["Enable"] = true, --[[ enable using skill V]]
-                    ["HoldTime"] = 3, --[[ hold skill in (seconds)]]
-                },
-                ["F"] = {
-                    ["Enable"] = false, --[[ enable using skill F]]
-                    ["HoldTime"] = 0, --[[ hold skill in (seconds)]]
-                },
-            },
-        },
-        ["Sword"] = {
-            ["Enable"] = true, --[[ enable using sword]]
-            ["Delay"] = 1, --[[ time delay between the sword skill ]]
-            ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = true, --[[ enable using skill Z]]
-                    ["HoldTime"] = 0, --[[ hold skill in (seconds)]]
-                },
-                ["X"] = {
-                    ["Enable"] = true, --[[[[enable using skill X]]
-                    ["HoldTime"] = 0, --[[ hold skill in (seconds)]]
-                },
-            },
-        },
-    }
-}
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Xero2409/XeroHub/main/bounty.lua")))()
+local function tween(place, item, boss, state, fruit)
+    repeat task.wait()
+        if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health <= 0 then
+            repeat task.wait(1) until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health > 0
+            
+        end
+        local player = game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+        if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Sit == true then
+            game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Sit = false
+        end
+        
+        
+        local Distance = (place.Position - player.Position).Magnitude
+        local speed = 300
+        if Distance < 250 then
+            speed = 5000
+        elseif Distance < 500 then
+            speed = 450
+        elseif Distance < 1000 then
+            speed = 350
+        elseif Distance >= 1000 then
+            speed = 300
+        end
+        local TweenService = game:GetService("TweenService")
+        local start = player.Position
+        local _end = place.Position
+        local distance = (start - _end).Magnitude
+        local _time = distance/(speed)
+        local info = TweenInfo.new(
+            _time,
+            Enum.EasingStyle.Linear
+        )
+        Tween = TweenService:Create(player, info, {CFrame = place})
+        Tween:Play()
+        if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health <= 0 then
+            repeat task.wait(1) until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health > 0
+            
+        end
+        if Stop_Tween then
+            break
+        end
+        if item ~= nil then
+            if item == "Third Sea" then
+                game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("TravelZou")
+            elseif not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild(item) and not game:GetService("Players").LocalPlayer.Character:FindFirstChild(item) then
+                
+                break
+            end
+        end
+        if boss ~= nil then
+            local CFrameBoss = FindBossCFrame(boss)
+            if CFrameBoss == nil then
+                
+                break
+            end
+        end
+        if state ~= nil then
+            if state == "Auto Farm Level" then
+                if not getgenv().Auto_Farm_Level then
+                    
+                    break
+                end
+            elseif state == "Auto Farm Boss" then
+                if not getgenv().Auto_Farm_Boss then
+                    
+                    break
+                end
+            elseif state == "Auto Farm Bone" then
+                if not getgenv().Auto_Farm_Bone then
+                    
+                    break
+                end
+            elseif state == "Auto Farm Kata" then
+                if not getgenv().Auto_Farm_Kata then
+                    
+                    break
+                end
+            elseif state == "Auto Bring Fruit" then
+                if not getgenv().Auto_Bring_Fruit then
+                    
+                    break
+                end
+            end
+        end
+        if fruit ~= nil then
+            local Check_Fruit = false
+            for i, v in pairs(game:GetService("Workspace"):GetChildren()) do
+                if v:FindFirstChild("Fruit") then
+                    if v.Name == fruit then
+                        Check_Fruit = true
+                    end
+                end
+            end
+            if not Check_Fruit then
+                
+                break
+            end
+        end
+    until Distance <= 10
+    RemoveVelocity()
+end
+tween(-1126.82251, 16.9443321, -14430.4238)

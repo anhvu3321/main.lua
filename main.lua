b13cf523a9b2a95f35636f48df9e0520f0570a94
anhvu3321 <117070253+anhvu3321@users.noreply.github.com>
2024-01-17 16:34:49 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,78 +1,61 @@
-if Setting then return end
-
 getgenv().Setting = {
-    ["Hunt"] = {
-        ["Team"] = "Marines"
-    },
+    ["Team"] = "Pirates",
     ["Webhook"] = {
-        ["Enable"] = false,
+        ["Enable"] = false, 
         ["Url"] = ""
     },
     ["Skip"] = {
-        ["V4"] = true,
-        ["Fruit"] = {
-            "Portal-Portal",
-            "Mammoth-Mammoth",
-            "Buddha-Buddha"
-        },
-        ["Near-Island Max Distance"] = 7000
+        ["Race V4"] = true,
+        ["Fruit"] = { 
+            "Leopard-Leopard",
+            "Venom-Venom",
+            "Dough-Dough",
+            "Portal-Portal"
+        }
     },
     ["Chat"] = {
-        ["Enable"] = true,
-        ["Content"] = "ez, gg, gggggg"
+        ["Enabled"] = false,
+        ["Content"] = {""},
     },
     ["Misc"] = {
-        ["Hold Until Max Skill Preserve"] = false,
-        ["Tweening On HoldTime"] = false,
-        ["Silent Mode"] = true,
+        ["Lock Bounty"] = {0, 30000000},
         ["Hide If Low Health"] = true,
-        ["Hiding Health"] = {4000, 8000},
-        ["V4"] = true,
-        ["LockCamera"] = false,
-        ["FPSBoost"] = false,
-        ["WhiteScreen"] = false,
-        ["BypassTP"] = true,
-        ["TweenSpeed"] = 350,
-        ["HopRegion"] = "Singapore"
+        ["Hide Health"] = {4000,5000},
+        ["Lock Camera"] = true,
+        ["FPS Boost"] = false,
+        ["White Screen"] = false,
+        ["Bypass TP"] = true, 
+        ["Spam All Skill On V4"] = false, 
+        ["Random Fruit & Store"] = true,
+        ["Random Suprise"] = true
     },
-    ["Items"] = {
-        ["Melee"] = {
-            ["Enable"] = true,
-            ["Delay"] = 4,
-            ["Skills"] = {
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = 0},
-                ["X"] = {["Enable"] = true, ["HoldTime"] = .2},
-                ["C"] = {["Enable"] = true, ["HoldTime"] = 0}
-                --   ["V"] = {["Enable"] = false, ["HoldTime"] = 0}
-            }
-        },
-        ["Blox Fruit"] = {
-            ["Enable"] = false,
-            ["Delay"] = 6,
-            ["Skills"] = {
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = 0},
-                ["X"] = {["Enable"] = true, ["HoldTime"] = 0},
-                ["C"] = {["Enable"] = true, ["HoldTime"] = 0},
-                ["V"] = {["Enable"] = true, ["HoldTime"] = .25},
-                ["F"] = {["Enable"] = false, ["HoldTime"] = 0}
-            }
-        },
-        ["Sword"] = {
-            ["Enable"] = true,
-            ["Delay"] = 4,
-            ["Skills"] = {
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = .8},
-                ["X"] = {["Enable"] = true, ["HoldTime"] = .2}
-            }
-        },
-        ["Gun"] = {
-            ["Enable"] = true,
-            ["Delay"] = 3,
-            ["Skills"] = {
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = 0},
-                ["X"] = {["Enable"] = false, ["HoldTime"] = 0}
-            }
-        }
+    ["Melee"] = {
+        ["Enable"] = true,
+        ["Delay"] = 3,
+        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["X"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["C"] = {["Enable"] = true, ["Hold Time"] = 0}
+    },
+    ["Fruit"] = {
+        ["Enable"] = false,
+        ["Delay"] = 2,
+        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["X"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["C"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["V"] = {["Enable"] = true, ["Hold Time"] = 1.25},
+        ["F"] = {["Enable"] = false, ["Hold Time"] = 0}
+    },
+    ["Sword"] = {
+        ["Enable"] = true,
+        ["Delay"] = 1,
+        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["X"] = {["Enable"] = true, ["Hold Time"] = 0}
+    },
+    ["Gun"] = {
+        ["Enable"] = true,
+        ["Delay"] = 1,
+        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["X"] = {["Enable"] = true, ["Hold Time"] = 0}
     }
 }
-loadstring(game:HttpGet([[https://raw.githubusercontent.com/eltrul/a/main/Autobounty-0.lua.txt]]))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vuquocoai123/lua/main/AutoBountyV2Beta"))()

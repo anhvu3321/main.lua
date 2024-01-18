39245474386dc51ca8e6be3a19cc961b99d4c0ab
anhvu3321 <117070253+anhvu3321@users.noreply.github.com>
2024-01-18 15:30:31 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,61 +1,63 @@
-getgenv().Setting = {
-    ["Team"] = "Pirates",
-    ["Webhook"] = {
-        ["Enable"] = false, 
-        ["Url"] = ""
-    },
-    ["Skip"] = {
-        ["Race V4"] = true,
-        ["Fruit"] = { 
-            "Leopard-Leopard",
-            "Venom-Venom",
-            "Dough-Dough",
-            "Portal-Portal"
-        }
-    },
-    ["Chat"] = {
-        ["Enabled"] = false,
-        ["Content"] = {""},
-    },
-    ["Misc"] = {
-        ["Lock Bounty"] = {0, 30000000},
-        ["Hide If Low Health"] = true,
-        ["Hide Health"] = {4000,5000},
-        ["Lock Camera"] = true,
-        ["FPS Boost"] = false,
-        ["White Screen"] = false,
-        ["Bypass TP"] = true, 
-        ["Spam All Skill On V4"] = false, 
-        ["Random Fruit & Store"] = true,
-        ["Random Suprise"] = true
-    },
-    ["Melee"] = {
-        ["Enable"] = true,
-        ["Delay"] = 3,
-        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
-        ["X"] = {["Enable"] = true, ["Hold Time"] = 0},
-        ["C"] = {["Enable"] = true, ["Hold Time"] = 0}
-    },
-    ["Fruit"] = {
-        ["Enable"] = false,
-        ["Delay"] = 2,
-        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
-        ["X"] = {["Enable"] = true, ["Hold Time"] = 0},
-        ["C"] = {["Enable"] = true, ["Hold Time"] = 0},
-        ["V"] = {["Enable"] = true, ["Hold Time"] = 1.25},
-        ["F"] = {["Enable"] = false, ["Hold Time"] = 0}
-    },
-    ["Sword"] = {
-        ["Enable"] = true,
-        ["Delay"] = 1,
-        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
-        ["X"] = {["Enable"] = true, ["Hold Time"] = 0}
-    },
-    ["Gun"] = {
-        ["Enable"] = true,
-        ["Delay"] = 3,
-        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
-        ["X"] = {["Enable"] = true, ["Hold Time"] = 0}
-    }
-}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vuquocoai123/lua/main/AutoBountyV2Beta"))()
+local function AddVelocity()
+    if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Xero") then
+        local body = Instance.new("BodyVelocity")
+        body.Name = "Xero"
+        body.Parent = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+        body.MaxForce=Vector3.new(1000000000,1000000000,1000000000)
+        body.Velocity=Vector3.new(0,0,0)
+    end
+end
+local function RemoveVelocity()
+    for _,v in pairs(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"):GetChildren()) do
+        if v.Name == "Xero" then
+            v:Destroy()
+        end
+    end
+end
+local function NoClip()
+    local character = game:GetService("Players").LocalPlayer.Character
+    for _, v in pairs(character:GetChildren()) do
+        if v:IsA("BasePart") then
+            v.CanCollide = false
+        end
+    end
+end
+local function tween(place, item, boss, state, fruit)
+    repeat task.wait()
+        if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health <= 0 then
+            repeat task.wait(1) until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health > 0
+            AddVelocity()
+        end
+        local player = game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+        AddVelocity()
+        NoClip()
+        local Distance = (place.Position - player.Position).Magnitude
+        local speed = 300
+        if Distance < 250 then
+            speed = 5000
+        elseif Distance < 500 then
+            speed = 450
+        elseif Distance < 1000 then
+            speed = 350
+        elseif Distance >= 1000 then
+            speed = 300
+        end
+        local TweenService = game:GetService("TweenService")
+        local start = player.Position
+        local _end = place.Position
+        local distance = (start - _end).Magnitude
+        local _time = distance/(speed)
+        local info = TweenInfo.new(
+            _time,
+            Enum.EasingStyle.Linear
+        )
+        Tween = TweenService:Create(player, info, {CFrame = place})
+        Tween:Play()
+        if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health <= 0 then
+            repeat task.wait(1) until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health > 0
+            AddVelocity()
+        end
+    until Distance <= 10
+    RemoveVelocity()
+end
+tween(CFrame.new(-16602.1015625, 130.38734436035156, 1087.24560546875))

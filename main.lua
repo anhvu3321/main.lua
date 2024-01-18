43ffde4ff16f09814205a68c2add16b58b4b336b
anhvu3321 <117070253+anhvu3321@users.noreply.github.com>
2024-01-18 15:32:53 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,63 +1,39 @@
-local function AddVelocity()
-    if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Xero") then
-        local body = Instance.new("BodyVelocity")
-        body.Name = "Xero"
-        body.Parent = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
-        body.MaxForce=Vector3.new(1000000000,1000000000,1000000000)
-        body.Velocity=Vector3.new(0,0,0)
-    end
-end
-local function RemoveVelocity()
-    for _,v in pairs(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"):GetChildren()) do
-        if v.Name == "Xero" then
-            v:Destroy()
+function GetLocalBoat()
+    for i, v in next, game:GetService("Workspace").Boats:GetChildren() do
+        if v:IsA("Model") then
+            if
+                v:FindFirstChild("Owner") and tostring(v.Owner.Value) == game:GetService("Players").LocalPlayer.Name and
+                    v.Humanoid.Value > 0
+                then
+                return v
+            end
         end
     end
 end
-local function NoClip()
-    local character = game:GetService("Players").LocalPlayer.Character
-    for _, v in pairs(character:GetChildren()) do
-        if v:IsA("BasePart") then
-            v.CanCollide = false
-        end
-    end
+function FlyBoat(boat,enable) 
+    if not boat then return end
+    if not enable then 
+        if boat:FindFirstChild("Fly Model") then 
+            boat:FindFirstChild("Fly Model"):Destroy()
+            return 
+        end  
+        if boat:FindFirstChild("Fly Model V2") then 
+            boat:FindFirstChild("Fly Model V2"):Destroy()
+            return 
+        end 
+    end 
+    
+    local bg = Instance.new("BodyGyro", boat)
+    bg.P = 9e4
+    bg.Name = "Fly Model V2"
+    bg.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+    bg.cframe = boat.VehicleSeat.CFrame
+    NoClipInstance = Instance.new("BodyVelocity",boat)
+    NoClipInstance.Name = "Fly Model"
+    NoClipInstance.P = 15000
+    NoClipInstance.MaxForce = Vector3.new(9e9, 9e9, 9e9)
+    NoClipInstance.Velocity = Vector3.new(0,0,0) 
+    NoClipInstance.CFrame = boat.VehicleSeat.CFrame
+    boat.VehicleSeat.CFrame = CFrame.new(boat.VehicleSeat.CFrame.X,boat.VehicleSeat.CFrame.Y+250,boat.VehicleSeat.CFrame.Z)
 end
-local function tween(place, item, boss, state, fruit)
-    repeat task.wait()
-        if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health <= 0 then
-            repeat task.wait(1) until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health > 0
-            AddVelocity()
-        end
-        local player = game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-        AddVelocity()
-        NoClip()
-        local Distance = (place.Position - player.Position).Magnitude
-        local speed = 300
-        if Distance < 250 then
-            speed = 5000
-        elseif Distance < 500 then
-            speed = 450
-        elseif Distance < 1000 then
-            speed = 350
-        elseif Distance >= 1000 then
-            speed = 300
-        end
-        local TweenService = game:GetService("TweenService")
-        local start = player.Position
-        local _end = place.Position
-        local distance = (start - _end).Magnitude
-        local _time = distance/(speed)
-        local info = TweenInfo.new(
-            _time,
-            Enum.EasingStyle.Linear
-        )
-        Tween = TweenService:Create(player, info, {CFrame = place})
-        Tween:Play()
-        if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health <= 0 then
-            repeat task.wait(1) until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").Health > 0
-            AddVelocity()
-        end
-    until Distance <= 10
-    RemoveVelocity()
-end
-tween(CFrame.new(-16602.1015625, 130.38734436035156, 1087.24560546875))
+FlyBoat(GetLocalBoat(), true)

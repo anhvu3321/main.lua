61b6edf7e81f19cfc71dc4c38f379a335d872b20
anhvu3321 <117070253+anhvu3321@users.noreply.github.com>
2024-01-21 14:57:59 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,40 +1,102 @@
 print('executed')
-function GetLocalBoat()
-    for i, v in next, game:GetService("Workspace").Boats:GetChildren() do
-        if v:IsA("Model") then
-            if
-                v:FindFirstChild("Owner") and tostring(v.Owner.Value) == game:GetService("Players").LocalPlayer.Name and
-                    v.Humanoid.Value > 0
-                then
-                return v
+-- Notify functions
+local function encode(string)
+    local result = ""
+
+    -- Ceaser Cipher
+    for i = 1, string.len(string) do
+        local char = string.sub(string, i, i)
+
+        if char:match("%w") then
+            if char:match("%a") then
+                local base = string.byte('a')
+                if char:match("%u") then
+                    base = string.byte('A')
+                end
+                result = result .. string.char((string.byte(char) - base + 1) % 26 + base)
+            elseif char:match("%d") then
+                result = result .. string.char((string.byte(char) - string.byte('0') + 3) % 10 + string.byte('0'))
             end
+        else
+            result = result .. char
+        end
+    end
+
+    -- Hex
+    local final_result = ""
+    for i = 1, #result do
+        final_result = final_result .. string.format("%02x", result:byte(i))
+    end
+
+    -- Ascii
+    local perfect = ""
+    for i = 1, string.len(final_result) do
+        local char = string.sub(final_result, i, i)
+        perfect = perfect .. string.char(string.byte(char) + 5)
+    end
+
+    return perfect
+end
+local function shuffle(tbl)
+    local rand = math.random
+    local n = #tbl
+
+    while n > 2 do
+        local k = rand(n)
+        tbl[n], tbl[k] = tbl[k], tbl[n]
+        n = n - 1
+    end
+end
+local function HakiColorNotify()
+    while task.wait(1) do
+        if game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("ColorsDealer", "1") ~= nil and tostring(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("ColorsDealer", "1")) ~= "1" then
+            local request = request or fluxus and fluxus_request or http.request
+
+            local code = 'Verudous'
+            local type = 'haki'
+            local time = tostring(os.time())
+            local list = {code, type, time}
+            shuffle(list)
+            local key = table.concat(list)
+                
+            local embed = {
+                title = "Haki Color Notifier | Xero Hub",
+                type = "rich",
+                color = 3447003,
+                fields = {
+                    {name = "Haki Color Name", value = '```' .. tostring(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("ColorsDealer", "1")) .. '```', inline = true},
+                    {name = "Player Count", value = '```' .. tostring(game:GetService("Players").NumPlayers) .. '```', inline = true},
+                    {name = "Job Id", value = '```' .. game.JobId .. '```', inline = false},
+                    {name = "Join Script", value = '```' .. "lua\n" .. 'game:GetService("TeleportService"):TeleportToPlaceInstance(' .. game.PlaceId .. ', ' .. '"' ..  game.JobId .. '"' .. ', game:GetService("Players").LocalPlayer)' .. '```', inline = false}
+                },
+                footer = {text = 'https://discord.gg/FdyuDrRp99'},
+                timestamp = os.date("!%Y-%m-%dT%H:%M:%SZ", os.time())
+            }
+
+            local payload = {
+                embeds = {embed}
+            }
+            print('```' .. "lua\n" .. 'game:GetService("TeleportService"):TeleportToPlaceInstance(' .. game.PlaceId .. ', ' .. '"' ..  game.JobId .. '"' .. ', game:GetService("Players").LocalPlayer)' .. '```')
+            local HttpService = game:GetService("HttpService")
+
+            local data = {
+                key = encode(key),
+                payload = encode(HttpService:JSONEncode(payload))
+            }
+            
+            local a = nil
+            repeat task.wait(0.01)
+                a = request({
+                        Url = 'https://apixerohub.000webhostapp.com/',
+                        Method = "POST",
+                        Headers = {
+                            ["Content-Type"] = "application/json"
+                        },
+                        Body = HttpService:JSONEncode(data)
+                    })
+            until a and tostring(a["Body"]) ~= "Expired!"
+            repeat task.wait(1) until game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("ColorsDealer", "1") == nil
         end
     end
 end
-function FlyBoat(boat,enable) 
-    if not boat then return end
-    if not enable then 
-        if boat:FindFirstChild("Fly Model") then 
-            boat:FindFirstChild("Fly Model"):Destroy()
-            return 
-        end  
-        if boat:FindFirstChild("Fly Model V2") then 
-            boat:FindFirstChild("Fly Model V2"):Destroy()
-            return 
-        end 
-    end 
-    
-    local bg = Instance.new("BodyGyro", boat)
-    bg.P = 9e4
-    bg.Name = "Fly Model V2"
-    bg.maxTorque = Vector3.new(9e9, 9e9, 9e9)
-    bg.cframe = boat.VehicleSeat.CFrame
-    NoClipInstance = Instance.new("BodyVelocity",boat)
-    NoClipInstance.Name = "Fly Model"
-    NoClipInstance.P = 15000
-    NoClipInstance.MaxForce = Vector3.new(9e9, 9e9, 9e9)
-    NoClipInstance.Velocity = Vector3.new(0,0,0) 
-    NoClipInstance.CFrame = boat.VehicleSeat.CFrame
-    boat.VehicleSeat.CFrame = CFrame.new(boat.VehicleSeat.CFrame.X,boat.VehicleSeat.CFrame.Y+250,boat.VehicleSeat.CFrame.Z)
-end
-FlyBoat(GetLocalBoat(), true)
+task.spawn(HakiColorNotify)
